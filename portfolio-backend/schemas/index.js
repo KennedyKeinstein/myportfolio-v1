b0e4e583d90f7b20b64e963335d052030f55392e
Keinnstein <kennedy.developper@gmail.com>
2023-04-11 23:19:29 +0300

    All Schemas

--- portfolio-backend/schemas/index.js
+++ portfolio-backend/schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brands from './brands'
+import contacts from './contacts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [
+  workExperience,
+  skills,
+  testimonials,
+  abouts,
+  brands,
+  contacts,
+  experiences,
+  works,
+]
