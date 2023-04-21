7b507342dce2cdb2aa343cac0f6d96fe00598959
Keinnstein <kennedy.developper@gmail.com>
2023-04-21 15:44:29 +0300

    Menu Li

--- portfolio-frontend/src/components/Navbar/Navbar.jsx
+++ portfolio-frontend/src/components/Navbar/Navbar.jsx
@@ -3,11 +3,21 @@ import "./Navbar.scss";
 import { images } from "../../constants";
 
 const Navbar = () => {
+  const items = ["home", "about", "work", "skills", "contact"];
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+
+      <ul className="app__navbar-links">
+        {items.map((item) => (
+          <li key={`link-${item}`} className="app__flex p-text">
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
