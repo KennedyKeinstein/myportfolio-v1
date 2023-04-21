ea21a5657f059cf7ebaa30a44df4d52eb9fb47aa
Keinnstein <kennedy.developper@gmail.com>
2023-04-21 16:24:04 +0300

    Mobile Menu Responsive

--- portfolio-frontend/src/components/Navbar/Navbar.jsx
+++ portfolio-frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const items = ["home", "about", "work", "skills", "contact"];
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = (value) => () => setToggle(value);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -18,6 +23,28 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={handleToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.7, ease: "easeOut" }}
+          >
+            <HiX onClick={handleToggle(false)} />
+            <ul>
+              {items.map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={handleToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
