545e65e8d9cb5b1a574ce71b360489dac02a1d19
Keinnstein <kennedy.developper@gmail.com>
2023-04-21 19:16:49 +0300

    Using my Own logo

--- portfolio-frontend/src/components/Navbar/Navbar.jsx
+++ portfolio-frontend/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.logo} alt="logo" />
+        <img src={images.my_logo} alt="logo" />
       </div>
 
       <ul className="app__navbar-links">
